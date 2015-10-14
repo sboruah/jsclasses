49847b10e0dbf1a57c5753e7b63f7546222717f6
aalto <aalto@Soujanyaas-MacBook-Pro.local>
2015-10-14 14:01:49 +0300

    submitting my code for the assigment of the day

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-console.log('hello world')
+console.log('Which is the deadliest out of the two?')
 //array or object and flow control
 
-var drinks = ['greetea', 'tea']
+var drinks = ['peacock mantis shrimp', 'killer whale']
 
 console.log(drinks)
 
 function whatDrinksDoIhave () {
-	if (drinks[0] === 'tea') {
-		console.log('I have tea & it is the first item')
+	if (drinks[0] === 'peacock mantis shrimp') {
+		console.log('Peacock mantis shrimp : first item.  can strike a prey with 1,500 Newtons of force within three-thousandths of a second with their raptorial appendages moving at the speed of a gunshot from a twenty-two caliber rifle.')
 	} 
-	if (drinks[1] === 'tea') {
-		console.log('I have tea & it is the second item')
+	if (drinks[1] === 'peacock mantis shrimp') {
+		console.log('Peacock mantis shrimp : first item.  can strike a prey with 1,500 Newtons of force within three-thousandths of a second with their raptorial appendages moving at the speed of a gunshot from a twenty-two caliber rifle.')
 	}
-	if (drinks[0] === 'coffee') {
-		console.log('I have coffee & it is the first item')
+	if (drinks[0] === 'killer whale') {
+		console.log('Killer whales: second item.Of the very few confirmed attacks on humans by wild killer whales, none have been fatal.')
 	} 
 	if (drinks[1] === 'coffee') {
-		console.log('I have coffee & it is the first item')
+		console.log('Killer whales : second item. Of the very few confirmed attacks on humans by wild killer whales, none have been fatal.')
 	} 
 
 }

9bba9f674c4e06e338c21017dea24f85896eab37
aalto <aalto@Soujanyaas-MacBook-Pro.local>
2015-10-14 14:05:51 +0300

    submission for today's assignment

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 console.log('Which is the deadliest out of the two?')
 //array or object and flow control
 
-var drinks = ['peacock mantis shrimp', 'killer whale']
+var animals = ['peacock mantis shrimp', 'killer whale']
 
-console.log(drinks)
+console.log(animals)
 
-function whatDrinksDoIhave () {
-	if (drinks[0] === 'peacock mantis shrimp') {
+function deadlyanimals () {
+	if (animals[0] === 'peacock mantis shrimp') {
 		console.log('Peacock mantis shrimp : first item.  can strike a prey with 1,500 Newtons of force within three-thousandths of a second with their raptorial appendages moving at the speed of a gunshot from a twenty-two caliber rifle.')
 	} 
-	if (drinks[1] === 'peacock mantis shrimp') {
+	if (animals[1] === 'peacock mantis shrimp') {
 		console.log('Peacock mantis shrimp : first item.  can strike a prey with 1,500 Newtons of force within three-thousandths of a second with their raptorial appendages moving at the speed of a gunshot from a twenty-two caliber rifle.')
 	}
-	if (drinks[0] === 'killer whale') {
+	if (animals[0] === 'killer whale') {
 		console.log('Killer whales: second item.Of the very few confirmed attacks on humans by wild killer whales, none have been fatal.')
 	} 
-	if (drinks[1] === 'coffee') {
+	if (animals[1] === 'coffee') {
 		console.log('Killer whales : second item. Of the very few confirmed attacks on humans by wild killer whales, none have been fatal.')
 	} 
 
 }
 
-whatDrinksDoIhave ()
+deadlyanimals ()
